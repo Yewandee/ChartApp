8d15e588b8d1ede5c234769d4ad3147863302e09
Yewandee <88537289+Yewandee@users.noreply.github.com>
2023-09-28 13:02:06 +0100

    commit

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -23,8 +23,11 @@ const Header = () => {
                         <ul class=";l-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
                                 <a class="nav-link" href='#'><span class="navbar-toggler-icon"></span></a>
-
                             </li>
+                            <li class="nav-item"> 
+                                
+                            </li>
+
                         </ul>
 
                         <div className="switch">
